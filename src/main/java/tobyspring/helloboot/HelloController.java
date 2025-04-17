a38e17f2b897d51070114ac4e7c162e8ff529855
jonghyeok-97 <dkswhdgur1209@naver.com>
2025-04-17 23:42:55 +0900

    스프링 컨테이너 사용

---
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -0,0 +1,8 @@
+package tobyspring.helloboot;
+
+public class HelloController {
+
+    public String hello(String target) {
+        return "Hello" + target;
+    }
+}
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -19,6 +20,11 @@ import org.springframework.http.MediaType;
 public class HellobootApplication {
 
 	public static void main(String[] args) {
+		// 스프링 컨테이너
+		GenericApplicationContext applicationContext = new GenericApplicationContext();
+		applicationContext.registerBean(HelloController.class); // 빈 등록
+		applicationContext.refresh(); // 빈 초기화
+
 		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
 			servletContext.addServlet("frontcontroller", new HttpServlet() {
@@ -27,19 +33,20 @@ public class HellobootApplication {
 						throws ServletException, IOException {
 					// 인증, 보안, 다국어, 공통 기능
 					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-
 						String name = req.getParameter("name");
 
-						resp.setStatus(HttpStatus.OK.value());
-						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-						resp.getWriter().println("hello servlet" + name);
-					} else if (req.getRequestURI().equals("/order")) {
-						//...
+						// 서블릿은 스프링컨테이너에서 만들어진 빈이 어떻게 생성되었는지도 모른채 가져다 쓰기만 한다.
+						HelloController helloController = applicationContext.getBean(HelloController.class);
+						String ret = helloController.hello(name);
+
+						resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
+						resp.getWriter().println(ret);
 					} else {
 						resp.setStatus(HttpStatus.NOT_FOUND.value());
 					}
 				}
 			}).addMapping("/*");
+
 		});
 
 		webServer.start();
