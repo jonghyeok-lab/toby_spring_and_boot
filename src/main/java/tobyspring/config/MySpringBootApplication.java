047bdaedc64c85b0bae97378479253fcd5bc3525
jonghyeok-97 <dkswhdgur1209@naver.com>
2025-04-22 00:09:35 +0900

    AutoConfiguration 활용

--- src/main/java/tobyspring/config/MySpringBootApplication.java
+++ src/main/java/tobyspring/config/MySpringBootApplication.java
@@ -1,4 +1,4 @@
-package tobyspring.helloboot;
+package tobyspring.config;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -11,5 +11,6 @@ import org.springframework.context.annotation.Configuration;
 @Target(ElementType.TYPE) // class interface enum
 @Configuration
 @ComponentScan
+@EnableMyAutoConfiguration
 public @interface MySpringBootApplication {
 }
--- src/main/java/tobyspring/config/autoconfig/DispatcherServletConfig.java
+++ src/main/java/tobyspring/config/autoconfig/DispatcherServletConfig.java
@@ -1,18 +1,11 @@
-package tobyspring.helloboot;
+package tobyspring.config.autoconfig;
 
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
-public class Config {
-
-    @Bean
-    public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
-    }
+public class DispatcherServletConfig {
 
     /**
      * ApplicationContextAware 를 구현
