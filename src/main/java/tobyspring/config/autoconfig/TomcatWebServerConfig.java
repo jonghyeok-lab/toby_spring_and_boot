92709c3f82921de1e43be44e5bc8e5aa64163018
jonghyeok-97 <dkswhdgur1209@naver.com>
2025-05-17 16:19:46 +0900

    스프링컨텍스트가 Import 후보군 중에서 조건에 따라 빈으로 등록할 수 있도록 @Conditional 추가
    - @Configuration 과 @Bean 에도 추가 가능

--- src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -3,10 +3,16 @@ package tobyspring.config.autoconfig;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 import tobyspring.config.MyAutoConfiguration;
+import tobyspring.config.autoconfig.TomcatWebServerConfig.TomcatCondition;
 
 @MyAutoConfiguration
+@Conditional(TomcatCondition.class) // 후보군들 중에서 빈으로 등록할지 말지 조건에 따라 스프링 컨테이너가 정할 수 있게 해줌
 public class TomcatWebServerConfig {
 
     @Bean("tomcatWebServerFactory")
@@ -15,4 +21,11 @@ public class TomcatWebServerConfig {
     }
 
 
+    static class TomcatCondition implements Condition {
+
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return false;
+        }
+    }
 }
